158b852f9bf92acc2f287706c41f7170e29bf318
suuuuwimmer@gmail.com <suuuuwimmer@gmail.com>
2025-01-01 23:39:01 +0900

    head first csharp 1 rearranged

---
+++ head-first-csharp/MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //출력
+        }
+    }
+}
+
